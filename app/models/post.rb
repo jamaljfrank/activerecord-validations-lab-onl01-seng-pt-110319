d853d84b9fc068e2bb2ca0cddb6d422db8ecb423
Jamal Frank <jamaljfrank@gmail.com>
2020-04-03 08:01:30 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,29 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non_Fiiction)}
+  validate :clickbait?
+  
+  
+
+
+private
+
+  CLICKBAIT_WORDS = [
+    "Won't Believe",
+    "Secret",
+    "Top [0-9]",
+    "Guess"
+  ]
+  
+  def bait_words
+    CLICKBAIT_WORDS.any? {|bait| title.include?(bait)}
+  end
+  
+  def clickbait?
+    unless bait words
+      errors.add(:title, "must be clickbait")
+    end  
+  end
 end
