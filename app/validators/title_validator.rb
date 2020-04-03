92dc3f30aba621be28695670c2d81238a55fb6f2
Jamal Frank <jamaljfrank@gmail.com>
2020-04-03 08:20:43 +0000

    Automatically backed up by Learn

--- app/validators/title_validator.rb
+++ app/validators/title_validator.rb
@@ -9,4 +9,7 @@ class TitleValidator < ActiveModel::Validator
   
   def validate(record)
     unless record.title.match?(CLICKBAIT_WORDS)
-      record.errors[:title] << 
+      record.errors[:title] << "Title must include clickbait words."
+    end
+  end
+end
